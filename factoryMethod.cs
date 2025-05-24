6c952dd70a4611b8c1da9b47e2cf08ae3a18de52
mqwl <141118776+Mqwelli@users.noreply.github.com>
2025-05-24 18:18:19 +1000

    factory method pattern implementation with c#

---
+++ factoryMethod.cs
@@ -0,0 +1,54 @@
+﻿using System;
+namespace Factory
+{
+    public interface IFormat
+    {
+        void Print();
+    }
+    public abstract class Archiver
+    {
+        public abstract IFormat CreateArchive();
+        public void NewArchive()
+        {
+            IFormat archive = CreateArchive();
+            archive.Print();
+        }
+    } 
+    public class Format7z : IFormat
+    {
+        public void Print() {
+            Console.WriteLine(".7z format archive");
+        }
+    }
+    public class FormatRar : IFormat
+    {
+        public void Print()
+        {
+            Console.WriteLine(".rar format archive");
+        }
+    }
+    public class Archiver7z : Archiver
+    {
+        public override IFormat CreateArchive()
+        {
+            return new Format7z();
+        }
+    }
+    public class ArchiverRar : Archiver
+    {
+        public override IFormat CreateArchive()
+        {
+            return new FormatRar();
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Archiver sevenZip = new Archiver7z();
+            sevenZip.NewArchive();
+            Archiver winRar = new ArchiverRar();
+            winRar.NewArchive();
+        }
+    }
+}
